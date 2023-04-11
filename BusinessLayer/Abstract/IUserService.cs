e43fd3f63e75907c20f17eeb581b85e88071ad28
Hsnakdag <75378924+Hsnakdag@users.noreply.github.com>
2023-04-11 15:02:51 +0300

    BusinessLayer

---
+++ BusinessLayer/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllUsers();
+        Task<User> GetUserById(int id);
+        Task<User> CreateUser(User user);
+        Task DeleteUserById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/UserService.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class UserService : IUserService
+    {
+        private readonly IBaseRepository _baseRepository;
+
+        public async Task<User> CreateUser(User user)
+        {
+           return await _baseRepository.CreateUser(user);
+        }
+
+        public async Task DeleteUserById(int id)
+        {
+            await _baseRepository.DeleteUserById(id);
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _baseRepository.GetAllUsers();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+           return await _baseRepository.GetUserById(id);
+        }
+    }
+}
