f92fcc0312259bcabfb1fce128693a192c8b7828
Hsnakdag <75378924+Hsnakdag@users.noreply.github.com>
2023-04-12 10:11:51 +0300

    jwtGenerator

--- BusinessLayer/Security/JwtManager.cs
+++ BusinessLayer/Security/JwtManager.cs
@@ -1,44 +1,45 @@
-﻿//using EntityLayer.Models;
-//using Microsoft.IdentityModel.Tokens;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Security.Claims;
-//using System.Text;
+﻿using EntityLayer.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-//namespace BusinessLayer.Security
-//{
-//    public class JwtManager : IJwtManager
-//    {
-//        private readonly JwtSettings _jwtSettings;
+namespace BusinessLayer.Security
+{
+    public class JwtManager : IJwtManager
+    {
+        private readonly IConfiguration _configuration;
 
-//        public JwtManager(JwtSettings jwtSettings)
-//        {
-//            _jwtSettings = jwtSettings;
-//        }
+        public JwtManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-//        public string GenerateJSONWebToken(User userInfo, Role roleInfo)
-//        {
-//            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings["Jwt:Key"]));
-//            if (securityKey.KeySize < 128)
-//            {
-//                securityKey = new SymmetricSecurityKey(new byte[16]); // Or some other method to generate a larger key
-//            }
-//            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        public string GenerateJSONWebToken(User userInfo, Role roleInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            if (securityKey.KeySize < 128)
+            {
+                securityKey = new SymmetricSecurityKey(new byte[16]); // Or some other method to generate a larger key
+            }
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-//            var claims = new[]
-//            {
-//            new Claim(ClaimTypes.Email, userInfo.Email.ToString()),
-//            new Claim(ClaimTypes.Role, roleInfo.RoleName),
-//            new Claim(JwtRegisteredClaimNames.Jti, userInfo.Password.ToString()),
-//        };
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.Email, userInfo.Email.ToString()),
+            new Claim(ClaimTypes.Role, roleInfo.RoleName),
+            new Claim(JwtRegisteredClaimNames.Jti, userInfo.Password.ToString()),
+        };
 
-//            var token = new JwtSecurityToken(
-//                issuer: _jwtSettings["Jwt:Issuer"],
-//                audience: _jwtSettings["Jwt:Audience"],
-//                claims: claims,
-//                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_jwtSettings["Jwt:ExpiryInMinutes"])),
-//                signingCredentials: credentials);
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"])),
+                signingCredentials: credentials);
 
-//            return new JwtSecurityTokenHandler().WriteToken(token);
-//        }
-//    }
-//}
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
